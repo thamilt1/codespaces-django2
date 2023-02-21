b20b876dc3d631bdba3b7b0cd35c744dfa1d3b70
EvanGiannini <121912502+EvanGiannini@users.noreply.github.com>
2023-02-21 00:39:49 +0000

    Message

---
+++ hello_world/Profile/base/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+  
+def home(request):
+    return render(request, "home.html")
+  
+def projects(request):
+    return render(request, "projects.html")
+  
+def contact(request):
+    return render(request, "contact.html")
+# Create your views here.
